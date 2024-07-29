4ba6f084b27e29261bb009aa9cd43615cd41143b
evgenGit81 <139344863+evgenGit81@users.noreply.github.com>
2024-07-29 17:34:03 +0000

    Update tests_12_1.py

--- tests_12_1.py
+++ tests_12_1.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 import unittest
-import runner2
+import runner
 
 class RannerTest(unittest.TestCase):
 
     def test_walk(self):
-        ds1 = runner2.Runner("Ivan")
+        ds1 = runner.Runner("Ivan")
         for i in range(10):
             ds1.walk()
         self.assertEqual(ds1.distance, 50)
 
     def test_run(self):
-        ds2 = runner2.Runner("Petr")
+        ds2 = runner.Runner("Petr")
         for i in range(10):
             ds2.run()
         self.assertEqual(ds2.distance, 100)
         
     def test_chellenge(self):
-        self.frst_r - runner2.Runner(name="Iiigor")
-        self.sec_r - runner2.Runner(name="Oleg")
+        self.frst_r - runner.Runner(name="Iiigor")
+        self.sec_r - runner.Runner(name="Oleg")
         for i in range(10):
             self.frst_r.run()
             self.sec_r.walk()
